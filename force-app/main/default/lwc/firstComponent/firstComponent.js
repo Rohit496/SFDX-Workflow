8130383b47e88111afcd64225aa190fe9efa7ec5
Rohit Kumar <rohitdotnet@outlook.com>
2021-06-28 21:11:53 +0530

    Second commit

---
+++ force-app/main/default/lwc/firstComponent/firstComponent.js
@@ -0,0 +1,13 @@
+/**
+ * @description       : 
+ * @author            : Rohit Kumar
+ * @group             : 
+ * @last modified on  : 06-28-2021
+ * @last modified by  : Rohit Kumar
+ * Modifications Log 
+ * Ver   Date         Author        Modification
+ * 1.0   06-28-2021   Rohit Kumar   Initial Version
+**/
+import { LightningElement } from "lwc";
+
+export default class FirstComponent extends LightningElement {}
