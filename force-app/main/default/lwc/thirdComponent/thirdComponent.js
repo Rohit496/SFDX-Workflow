f917d8bf9a5825ab11087b85b0bfa10ff3a6a117
Rohit Kumar <rohitdotnet@outlook.com>
2021-06-29 15:38:17 +0530

    my comment

--- force-app/main/default/lwc/thirdComponent/thirdComponent.js
+++ force-app/main/default/lwc/thirdComponent/thirdComponent.js
@@ -1,5 +1,13 @@
-import { LightningElement } from 'lwc';
+/**
+ * @description       : 
+ * @author            : Rohit Kumar
+ * @group             : 
+ * @last modified on  : 06-29-2021
+ * @last modified by  : Rohit Kumar
+ * Modifications Log 
+ * Ver   Date         Author        Modification
+ * 1.0   06-29-2021   Rohit Kumar   Initial Version
+**/
+import { LightningElement } from "lwc";
 
-export default class ThirdComponent extends LightningElement {
-    
-}
+export default class ThirdComponent extends LightningElement {}
